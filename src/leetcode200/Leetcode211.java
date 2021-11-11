c02c1d1171e801cf53ddd0b803c7d2cbb334168f
郭卿 <guoqing@guoqingdeMacBook-Pro.local>
2021-11-11 23:17:32 +0800

    fix leeetcode 211

--- src/leetcode200/Leetcode211.java
+++ src/leetcode200/Leetcode211.java
@@ -8,58 +8,60 @@ public class Leetcode211 {
     }
     
     public void addWord(String word) {
-    	node.insertWord(word);
+        node.insertWord(word);
+    }
+
+    public boolean search(String word) {
+        return node.dfs(node, word, 0);
     }
     
-    public boolean search(String word) {
-    	return node.findWord(word);
-    }
+
     
     class Trie {
-    	
-    	Trie[] childNode;
-    	boolean isEnd;
-    	boolean isFind = false;
-    	
-    	Trie() {
-    		childNode = new Trie[26];
-    		isEnd = false;
-    	}
-    	
-    	void insertWord(String word) {
-    		Trie node = this;
-    		for (int i = 0; i < word.length(); i++) {
-    			int index = word.charAt(i) - 'a';
-    			if (node.childNode[index] == null) {
-    				node.childNode[index] = new Trie();
-    			}
-    			node = node.childNode[index];
-    		}
-    		node.isEnd = true;
-    	}
-    	
-    	void dfs(Trie node, String word, int index) {
-    		if (node == null) {
-    			return;
-    		}
-    		
-    		// the edge condition
-    		if (node !=null && node.isEnd == true && index == word.length() && 
-    				(word.charAt(index - 1) == '.' || node.childNode[word.charAt(index - 1) - 'a'] != null)) {
-    			isFind = true;
-    			return;
-    		}
-    		
-    		for (Trie elem : node.childNode) {
-				dfs(elem, word, index++);
-				index--;   				
-    		}
-    	}
-    	
-    	boolean findWord(String word) {
-    		dfs(this, word, 0);
-    		return isFind;
-    	}
-    }
+
+        Trie[] childNode;
+        boolean isEnd;
+        boolean isFind = false;
+
+        Trie() {
+            childNode = new Trie[26];
+            isEnd = false;
+        }
+
+        void insertWord(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                int index = word.charAt(i) - 'a';
+                if (node.childNode[index] == null) {
+                    node.childNode[index] = new Trie();
+                }
+                node = node.childNode[index];
+            }
+            node.isEnd = true;
+        }
+        
+        boolean dfs(Trie node, String word, int index) {
+            if (index == word.length()) {
+                return node.isEnd;
+            }
+
+            // the edge condition
+            if (word.charAt(index) != '.') {
+                //
+                if (node.childNode[word.charAt(index) - 'a'] != null
+                        && dfs(node.childNode[word.charAt(index) - 'a'], word, index+1)) {
+                    return true;
+                }
+            } else {
+                // node.allchild != null can dfs
+                for (int i = 0; i < 26; i++) {
+                    if (node.childNode[i] != null && dfs(node.childNode[i], word, index+1)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    } 
 }
 
